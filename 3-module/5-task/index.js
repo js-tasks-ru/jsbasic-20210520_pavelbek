f4755a9a3716cb9bc01022710b722fe0cbd2fe68
Pavel <pavel.argunoff@gmail.com>
2021-06-02 19:21:07 +0300

    3-module tasks solved

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,15 @@
 function getMinMax(str) {
-  // ваш код...
+	
+	let elements = str.split(/[\s,]+/)
+		.filter(el => !isNaN(el))
+		.map(el => parseFloat(el))
+		.sort((a, b) => a - b)
+
+	if( elements.legth == 0) {
+	return {}
+	} else if(elements.legth < 2) {
+	return {min: elements[0], max: elements[0]}
+	}
+
+	return {min: elements[0], max: elements[elements.length - 1]};
 }
