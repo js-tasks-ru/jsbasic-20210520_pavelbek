edabfcafca2d56457cb530984b7413174256ba98
Pavel <pavel.argunoff@gmail.com>
2021-06-08 22:07:10 +0300

    solved tasks for 4-module

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,23 @@
 function highlight(table) {
-  // ваш код...
+
+	let rows = table.rows;
+	const age = 1;
+	const gender = 2;
+	const status = 3;
+
+	for (let i = 1; i < rows.length; i++) {
+
+		let cells = rows[i].children;
+
+		parseInt(cells[age].textContent) < 18 ? rows[i].style.textDecoration = "line-through" : null;
+
+		cells[gender].textContent === "m" ? rows[i].classList.add("male") : rows[i].classList.add("female");
+
+		if (cells[status].hasAttribute("data-available")) {
+			cells[status].dataset.available === "true" ? rows[i].classList.add("available") : rows[i].classList.add("unavailable");
+		} else {
+			rows[i].hidden = true;
+		}
+	}
+
 }
