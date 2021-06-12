6ea3fb23218015e07d1c20ffab61c652fd364e11
Pavel <pavel.argunoff@gmail.com>
2021-06-12 08:27:04 +0300

    5-module tasts solved

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
+
+	const toggleTextButton = document.querySelector('.toggle-text-button');
+	const elementToHide = document.getElementById('text');
+
+	toggleTextButton.addEventListener('click', () => elementToHide.hidden = !elementToHide.hidden)
 }
